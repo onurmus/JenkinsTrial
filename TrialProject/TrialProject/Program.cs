a85c991bdd2362b5ceb2b89e5ebaf5ad283d22b4
onur musaoğlu <aztech_onur@hotmail.com>
2018-10-24 17:31:33 +0300

    EG-5 #comment changelog trial

--- TrialProject/TrialProject/Program.cs
+++ TrialProject/TrialProject/Program.cs
@@ -18,6 +18,7 @@ namespace TrialProject
 			Console.WriteLine("5");
             Console.WriteLine("ETL geliştirmeleri 1. step");
             Console.WriteLine("Jira-Jenkins integration test");
+            Console.WriteLine("changelog trial");
 
             string json = JsonConvert.SerializeObject("here is a library trial");
             Console.WriteLine(json);
